32f1d54ce77e06626f47d19f308a9efdbc0bc39e
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-05-19 11:47:16 -0300

    codigoCompleto

---
+++ Assets/rstrtOnFall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class rstrtOnFall : MonoBehaviour
+{
+    public float yFakkHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < yFakkHeight)
+        {
+            Debug.Log("JAJA te caíste");
+            SceneManager.LoadScene("parkour");
+        }
+    }
+}
