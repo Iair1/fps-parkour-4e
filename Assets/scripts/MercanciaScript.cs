d8bd89f91ba70dce7e6c231c719b8afe85f4741e
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-05-28 15:51:00 -0300

    collider desaparicion

---
+++ Assets/scripts/MercanciaScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public int precio;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
